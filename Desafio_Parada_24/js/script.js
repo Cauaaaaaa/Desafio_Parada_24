ac15114c62fa6092b4853c4bdb9659053d3774c0
CauãZão <163067312+Cauaaaaaa@users.noreply.github.com>
2024-05-27 21:05:36 -0300

    Add files via upload

---
+++ Desafio_Parada_24/js/script.js
@@ -0,0 +1,20 @@
+var div = document.getElementById("resposta");
+
+function conversão(){
+
+    let celsius = parseFloat(document.getElementById("iCelsius").value);
+    let operador = document.getElementById("select").value;
+    let text;
+
+    switch (operador){
+
+        case "Fahrenheit":
+            text = `<p> ${celsius} * ${9/5} + ${32} = ${(celsius*(9/5)) + 32} </p>`;
+            break;
+        case "Kelvin":
+            text = `<p> ${celsius} + ${273.15} = ${celsius+273.15} K </p>`;
+
+    }
+    div.innerHTML = text;
+}
+
